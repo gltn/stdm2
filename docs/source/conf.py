5155b01a5aa8a378796248d85e0e77dfe1d03a5a
kngeno <kngeno.kevin@gmail.com>
2019-05-06 23:43:38 +0300

    add docs/build

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -63,8 +63,8 @@ html_theme_options = {
     'display_version': True,
     'prev_next_buttons_location': 'bottom',
     'style_external_links': False,
-    'vcs_pageview_mode': '',
-    'style_nav_header_background': 'white',
+    # 'vcs_pageview_mode': '',
+    # 'style_nav_header_background': 'white',
     # Toc options
     'collapse_navigation': True,
     'sticky_navigation': True,
