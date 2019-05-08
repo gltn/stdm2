f2d0352209b48dc5052b9d5f710ec842103c3cca
kngeno <kngeno.kevin@gmail.com>
2019-05-08 21:21:41 +0300

    test ftp

---
+++ scripts/deploy_ncftp.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+if  [[ $TRAVIS_PULL_REQUEST = "false" ]]
+then
+    ncftp -u "$USERNAME" -p "$PASSWORD" "$HOST"<<EOF
+    rm -rf site/wwwroot
+    mkdir site/wwwroot
+    quit
+EOF
+
+    # cd _site || exit
+    ncftpput -R -v -u "$USERNAME" -p "$PASSWORD" "$HOST" /master/ .
+fi
